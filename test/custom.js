077722eafc28cccd68b9456b013a75a1e731d102
Benjamin Pannell <admin@sierrasoftworks.com>
2014-03-21 01:19:08 +0200

    Improved error message handling

---
+++ test/custom.js
@@ -0,0 +1,11 @@
+﻿var should = require('should');
+var skmatc = require('../index.js');
+
+describe('custom validation', function() {
+    it('should allow instance specific registrations', function() {
+        var s = new skmatc('positive');
+        s.register(skmatc.create(function(schema) { return schema == 'positive'; }, function(schema, data) { return this.assert(data >= 0); }));
+        s.validate(1).success.should.be.true;
+        s.validate(-1).failed.should.be.true;
+    });
+});
